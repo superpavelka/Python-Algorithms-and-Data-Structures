c382aec07aa70ed8b16e8927c02941b74414b575
krilatiy <pos@list.ru>
2019-09-27 16:57:36 +0300

    les01: task2, new scheme for 1.1

---
+++ les01/les01_task02.py
@@ -0,0 +1,34 @@
+'''
+2. По введенным пользователем координатам двух точек вывести уравнение прямой вида y = kx + b, проходящей через эти точки.
+'''
+
+correct_input = False
+
+while not correct_input:
+    try:
+        print('Введите координаты первой точки x1 и y1')
+        x1 = int(input('x1= '))
+        y1 = int(input('y1= '))
+        print('Введите координаты второй точки x2 и y2')
+        x2 = int(input('x2= '))
+        y2 = int(input('y2= '))
+        correct_input = True
+    except ValueError:
+        print('Ошибка ввода!')
+
+if x1 != x2:
+    k = (y2 - y1) / (x2 - x1)
+    b = y1 - k * x1
+    if k == 1 and b != 0:
+        print(f'Уравнение линейной функции: y = x + {b}')
+    elif k == 1 and b == 0:
+        print(f'Уравнение линейной функции: y = x')
+    elif k != 0 and b != 0:
+        print(f'Уравнение линейной функции: y = {k}x + {b}')
+    elif k == 0 and b != 0:
+        print(f'Уравнение линейной функции: y = {b}')
+    elif k != 0 and b == 0:
+        print(f'Уравнение линейной функции: y = {k}x')
+
+else:
+    print(f'Уравнение линейной функции: x = {x1}')
