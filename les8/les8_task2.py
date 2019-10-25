2ae71264abdc35b7990245d7fd76ce6a02461108
krilatiy <pos@list.ru>
2019-10-25 15:04:08 +0300

    les8: 8.2 vertex check

--- les8/les8_task2.py
+++ les8/les8_task2.py
@@ -6,46 +6,49 @@
 
 def dijkstra(graph, start):
     length = len(graph)
-    is_visited = [False] * length
-    cost = [float('inf')] * length
-    parent = [-1] * length
-    parents = []
-    for i in range(length):
-        parents.append([])
-    spam_start = start
-
-    cost[start] = 0
-    min_cost = 0
-
-    while min_cost < float('inf'):
-
-        is_visited[start] = True
-
-        for i, vertex in enumerate(graph[start]):
-            if vertex != 0 and not is_visited[i]:
-                if cost[i] > vertex + cost[start]:
-                    cost[i] = vertex + cost[start]
-                    parent[i] = start
-                    if parents[start] != [] and parents[start] not in parents[i]:
-                        for p in parents[start]:
-                            if p not in parents[i]:
-                                parents[i].append(p)
-                    parents[i].append(start)
-
-        min_cost = float('inf')
+    if (start < length):
+        is_visited = [False] * length
+        cost = [float('inf')] * length
+        parent = [-1] * length
+        parents = []
         for i in range(length):
-            if min_cost > cost[i] and not is_visited[i]:
-                min_cost = cost[i]
-                start = i
+            parents.append([])
+        spam_start = start
 
-    parents[spam_start].append('begin')
-    for i, p in enumerate(parents):
-        if not p:
-            parents[i] = ['unreachable']
-    result = []
-    result.append(cost)
-    result.append(parents)
+        cost[start] = 0
+        min_cost = 0
 
+        while min_cost < float('inf'):
+
+            is_visited[start] = True
+
+            for i, vertex in enumerate(graph[start]):
+                if vertex != 0 and not is_visited[i]:
+                    if cost[i] > vertex + cost[start]:
+                        cost[i] = vertex + cost[start]
+                        parent[i] = start
+                        if parents[start] != [] and parents[start] not in parents[i]:
+                            for p in parents[start]:
+                                if p not in parents[i]:
+                                    parents[i].append(p)
+                        parents[i].append(start)
+
+            min_cost = float('inf')
+            for i in range(length):
+                if min_cost > cost[i] and not is_visited[i]:
+                    min_cost = cost[i]
+                    start = i
+
+        parents[spam_start].append('begin')
+        for i, p in enumerate(parents):
+            if not p:
+                parents[i] = ['unreachable']
+        result = []
+        result.append(cost)
+        result.append(parents)
+    else:
+        print('Нет такой вершины!')
+        result = [[],[]]
     return result
 
 
@@ -67,6 +70,7 @@ if __name__ == "__main__":
         except ValueError:
             print('Ошибка ввода!')
             continue
-        print(f'Стоимости вершин: {dijkstra(g, s)[0]}', sep='\n')
-        print(f'Пути к вершинам: {dijkstra(g, s)[1]}', sep='\n')
+        result = dijkstra(g, s)
+        print(f'Стоимости вершин: {result[0]}', sep='\n')
+        print(f'Пути к вершинам: {result[1]}', sep='\n')
         break
